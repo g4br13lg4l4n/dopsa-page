5aa11007bd7b1f7372d7eb229634c31ac9e5fea9
Gabriel Galan <gabriel.galan92@gmail.com>
2019-02-10 00:53:05 -0600

    add https

--- src/API/config.js
+++ src/API/config.js
@@ -1,6 +1,6 @@
 import isLocal from '../plugin/isLocal'
 
-const url = isLocal ? 'http://localhost:8000/api/' : 'http://dev.system.grupodopsa.com/api/'
+const url = isLocal ? 'http://localhost:8000/api/' : 'https://dev.system.grupodopsa.com/api/'
 const configService = {
   apiUrl: url
 }
